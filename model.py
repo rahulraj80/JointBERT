0b97b55ac39eafda88af29769904af0192a1d737
rahulraj80 <rahulraj80@users.noreply.github.com>
2020-02-21 10:53:03 +0530

    Changed import
    This import works on Kaggle : The earlier reference to torchcrf (instead of TorchCRF) gives an error

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from transformers import BertPreTrainedModel, BertModel, DistilBertModel, AlbertModel, DistilBertPreTrainedModel
-from torchcrf import CRF
+from TorchCRF import CRF
 
 
 PRETRAINED_MODEL_MAP = {
